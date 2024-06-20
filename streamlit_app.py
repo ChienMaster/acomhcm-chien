23094de4e13fe3af9e1e05b4fa8b9b3180d2caee
ChienMaster <131136108+ChienMaster@users.noreply.github.com>
2024-06-20 13:17:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -16,21 +17,18 @@ with st.expander('Giới thiệu về app'):
   
 st.subheader('WELCOME TO ACOM CORPORATION OLT CONFIGURATION WEBSITE!')
 
-tabs = st.tabs(['Cấu hình', 'Check thông tin dự án'])
+# tabs = st.tabs(['Cấu hình', 'Check thông tin dự án'])
 
-with tabs[0]:
-    project_options = ['Vinhomes Grand Park PK2', 'Vinhomes Grand Park PK3', 'Lavida Plus', 'Lux5 Bason', 'EcoxuanBD', 'TCL Tham Lương', 'HP One', 'AriaVT', 'BaryaVT', 'Hacom', 'Global City', 'Vin Hưng Yên', 'Vin Tây Mỗ', 'Vin Bắc Giang']
-    selected_project = st.selectbox('Chọn dự án', project_options)
+#with tabs[0]:
+project_options = ['Vinhomes Grand Park PK2', 'Vinhomes Grand Park PK3', 'Lavida Plus', 'Lux5 Bason', 'EcoxuanBD', 'TCL Tham Lương', 'HP One', 'AriaVT', 'BaryaVT', 'Hacom', 'Global City', 'Vin Hưng Yên', 'Vin Tây Mỗ', 'Vin Bắc Giang']
+selected_project = st.selectbox('Chọn dự án', project_options)
 
-    if selected_project:
-        options = ['Config', 'Check', 'Clear', 'Reboot', 'Bridge', 'thoai', 'iptv']
-        st.write(f'Bạn đã chọn dự án: {selected_project}')
-        st.write('Đang chuyển đến giao diện cấu hình OLT...')
-        congthuc = st.text_area('Nhập công thức theo hướng dẫn của Chiến ACOM (Nếu không biết vui lòng liên hệ: 0932277923 Phone/Zalo Chiến ACOM)')
-        if selected_project == 'Vinhomes Grand Park PK2':
-            choice = st.selectbox('Bạn muốn gì?', options)
-            if choice:
-                st.write('Bạn muốn {} OLT Vinhomes Grand Park PK2'.format(choice))
-
-with tabs[1]:
-    selected_project = st.selectbox('Chọn dự án', project_options)
+if selected_project:
+    options = ['Config', 'Check', 'Clear', 'Reboot', 'Bridge', 'thoai', 'iptv']
+    st.write(f'Bạn đã chọn dự án: {selected_project}')
+    st.write('Đang chuyển đến giao diện cấu hình OLT...')
+    congthuc = st.text_area('Nhập công thức theo hướng dẫn của Chiến ACOM (Nếu không biết vui lòng liên hệ: 0932277923 Phone/Zalo Chiến ACOM)')
+    if selected_project == 'Vinhomes Grand Park PK2':
+        choice = st.selectbox('Bạn muốn gì?', options)
+        if choice:
+            st.write('Bạn muốn {} OLT Vinhomes Grand Park PK2'.format(choice))
